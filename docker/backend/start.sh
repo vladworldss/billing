fbb37491479a4b79a70b3b98acc378abc1e9b77c
Vladimir Gerasimenko <v_gerasimenko@wargaming.net>
2021-04-29 00:58:30 +0300

    fix rabbitmq container

--- docker/backend/start.sh
+++ docker/backend/start.sh
@@ -4,6 +4,6 @@ service ssh start
 
 mkdir -p logs
 
-invoke init_config --db-connection="$DB_CONNECTION"  --redis-connection "$REDIS_CONNECTION" --amqp-userinfo "$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS" --silent
+#invoke init_config --db-connection="$DB_CONNECTION"  --redis-connection "$REDIS_CONNECTION" --amqp-userinfo "$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS" --silent
 
-uvicorn -b 0.0.0.0:8000 app:app --reload
+uvicorn --host 0.0.0.0 --port 8000 app:app --reload
